fe0efe09d44a369e5b4447ab0a843e605caeac0d
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-02-21 20:22:27 +0100

    Hitframes, force, framefreeze

---
+++ Development Lab 1 LJ2 P3/Assets/Wouter/CollisionChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour {
+    public bool collide;
+    public GameObject hitObject;
+
+    public void OnTriggerStay(Collider other)
+    {
+        print("test");
+        collide = true;
+        hitObject = other.gameObject;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        collide = false;
+        hitObject = null;
+        
+    }
+
+
+}
