f3b6a61aa8804733d803f4f311a0dd5856a0c30c
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-02-25 21:16:17 +0100

    *New* Weapon Inertia, *bug* Block grip broke

---
+++ Development Lab 1 LJ2 P3/Assets/Wouter/Wouter Scripts/Inertia.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inertia : MonoBehaviour {
+
+    public bool canSway;
+
+    public float amount;
+    public float maxAmount;
+    public float smoothAmount;
+
+    public Rigidbody playerbody;
+    public float yVelocity;
+
+    //public Vector3 offset;
+    private Vector3 initialPosition;
+    
+
+    private void Awake()
+    {
+        initialPosition = transform.localPosition;
+        
+    }
+
+    private void Update()
+    {
+        yVelocity = playerbody.velocity.y;
+
+        float movementX = -Input.GetAxis("Mouse X") * amount + -Input.GetAxis("Horizontal") * amount;
+        float movementY = -Input.GetAxis("Mouse Y") * amount + -yVelocity * amount;
+        float movementZ = -Input.GetAxis("Vertical") * amount * 0.5f;
+
+        if (!canSway)
+        {
+            movementX = 0;
+            movementY = 0;
+            movementZ = 0;
+        }
+
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
+        movementZ = Mathf.Clamp(movementZ, -maxAmount, maxAmount * 0.5f);
+
+        Vector3 finalPosition = new Vector3(movementX, movementY, movementZ);
+        
+        transform.localPosition = Vector3.Slerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * smoothAmount);
+
+
+
+
+        if (Input.GetKeyDown("k"))
+        {
+            canSway = !canSway;
+        }
+
+
+
+
+    }
+}
