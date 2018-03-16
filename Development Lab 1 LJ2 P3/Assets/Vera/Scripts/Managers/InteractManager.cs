f38adeb8c2fdd21980c42323a23681292e90de22
VeraGerritseDeltion <arev_97@live.nl>
2018-03-16 12:30:05 +0100

    Doors

--- Development Lab 1 LJ2 P3/Assets/Vera/Scripts/Managers/InteractManager.cs
+++ Development Lab 1 LJ2 P3/Assets/Vera/Scripts/Managers/InteractManager.cs
@@ -18,16 +18,21 @@ public class InteractManager : MonoBehaviour {
     {
         if (actions != null)
         {
+            print("1");
             if (actions.anim != null)
             {
+                print("2");
                 if (actions.anim.GetCurrentAnimatorStateInfo(2).IsTag("lol"))
                 {
+                    print("3");
                     RaycastHit hit;
                     if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * 3, out hit, 3, layers))
                     {
+                        print("test");
                         if (hit.collider.gameObject != null)
                         {
-                            if (hit.collider.gameObject.tag == "Interactable")
+                            print(hit.collider.gameObject.tag);
+                            if (hit.collider.gameObject.tag == "Interactable" || hit.collider.gameObject.tag == "FrontDoor" || hit.collider.gameObject.tag == "BackDoor")
                             {
                                 UIManager.instance.interfaceGame.inter = true;
                             }
