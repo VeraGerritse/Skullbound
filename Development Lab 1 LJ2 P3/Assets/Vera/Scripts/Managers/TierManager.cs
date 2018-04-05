238d92acb1f084db718b47c198cb72f8a083814d
unknown <arev_97@live.nl>
2018-04-05 15:36:13 +0200

    startLoot

---
+++ Development Lab 1 LJ2 P3/Assets/Vera/Scripts/Managers/TierManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TierManager : MonoBehaviour {
+
+    public static TierManager instance;
+    public int multiplier;
+    public int Tier;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
